a3e5fd5ae2c60a989ccd2b72dbb0b31875276a55
yibing <472621811@qq.com>
2015-10-28 16:55:58 +0800

    o Signed-off-by:yibing <472621811@qq.com>

--- application/controllers/Construct.php
+++ application/controllers/Construct.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class PageDemo extends CI_Controller {
+class Construct extends CI_Controller {
 
     public function index() {
         var_dump($_POST);
