111182881a9b317f076be04ab8815c65e38f4e28
yibing <472621811@qq.com>
2015-10-28 12:40:40 +0800

    o Signed-off-by:yibing <472621811@qq.com>

--- application/controllers/Foo.php
+++ application/controllers/Foo.php
@@ -1,32 +1,32 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Foo extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     * 	- or -
+     * 		http://example.com/index.php/welcome/index
+     * 	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index() {
 //                echo "hello world";
-		$this->load->view('foo_message');
-	}
-        
-        public function foo($name,$id)
-        {
-            echo $name;
-            echo $id;
-        }
+        $this->load->view('foo_message');
+    }
+
+    public function shoes($sandals, $id) {
+        echo $sandals;
+        echo $id;
+    }
+
 }
