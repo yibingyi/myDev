2b8b7a075c9839ac81ae59d99048704c059f4e27
yibing <472621811@qq.com>
2015-10-28 16:53:45 +0800

    o Signed-off-by:yibing <472621811@qq.com>

--- application/views/page_demo.php
+++ application/views/page_demo.php
@@ -7,14 +7,14 @@
     </head>
     <body>
         <form action="/index.php?/construct" method="post">
-        <span>raido_type</span><input type="radio" id = 'r_e_type' class = 'e_c_name' value = 'r_e'/><br>
-        <span>num&nbsp; </span><input type="text" id = 'r_e_num' class = 'e_c_number'/><br>
-        <span>action</span>
-        <select  id = 'r_e_action' class = 'e_c_action' name = 'r_action_opt'>
-            <option  value = '1'>relate_1</option>
-            <option  value = '2'>relate_2</option>
-        </select>
-        <input type="submit"/>
+            <span>raido_type</span><input type="radio" id = 'r_e_type' class = 'e_c_name' value = 'radio' name= 'type'/><br>
+            <span>num&nbsp; </span><input type="text" id = 'r_e_num' class = 'e_c_number' name = 'num'/><br>
+            <span>action</span>
+            <select  id = 'r_e_action' class = 'e_c_action' name = 'action'>
+                <option  value = '1'>relate_1</option>
+                <option  value = '2'>relate_2</option>
+            </select>
+            <input type="submit"/>
         </form>
     </body>
 </html>
