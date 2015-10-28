39b1f50d854e6b7cc85d65098e2fde9995ded6ae
yibing <472621811@qq.com>
2015-10-28 16:51:00 +0800

    o Signed-off-by:yibing <472621811@qq.com>

--- application/views/page_demo.php
+++ application/views/page_demo.php
@@ -8,7 +8,7 @@
     <body>
         <form action="/index.php?/construct" method="post">
         <span>raido_type</span><input type="radio" id = 'r_e_type' class = 'e_c_name' value = 'r_e'/><br>
-        <span>num&nbsp; &nbsp; &nbsp;</span><input type="text" id = 'r_e_num' class = 'e_c_number'/><br>
+        <span>num&nbsp; </span><input type="text" id = 'r_e_num' class = 'e_c_number'/><br>
         <span>action</span>
         <select  id = 'r_e_action' class = 'e_c_action' name = 'r_action_opt'>
             <option  value = '1'>relate_1</option>
