5f5c7c8cf868097a5d379959f1dfa220074a2c9d
yibing <472621811@qq.com>
2015-10-28 16:48:09 +0800

    o Signed-off-by:yibing <472621811@qq.com>

--- application/controllers/PageDemo.php
+++ application/controllers/PageDemo.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Foo extends CI_Controller {
+class PageDemo extends CI_Controller {
 
     public function index() {
         $this->load->view('page_demo');
