3d5d2d87e7d0b70a531ada75810288b86ebdc364
yibing <472621811@qq.com>
2015-10-28 16:46:48 +0800

    o Signed-off-by:yibing <472621811@qq.com>

---
+++ application/views/page_demo.php
@@ -0,0 +1,23 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+    <head>
+        <title>demo</title>
+    </head>
+    <body>
+        <form action="/index.php?/construct" method="post">
+        <span>raido_type</span><input type="radio" id = 'r_e_type' class = 'e_c_name' value = 'r_e'/><br>
+        <span>num</span><input type="text" id = 'r_e_num' class = 'e_c_number'/><br>
+        <span>action</span>
+        <input type="select " id = 'r_e_action' class = 'e_c_action' name = 'r_action_opt'>
+            <option  value = '1'>relate_1</option>
+            <option  value = '2'>relate_2</option>
+        </input>
+        <input type="submit"/>
+        </form>
+    </body>
+</html>
+
+
+
