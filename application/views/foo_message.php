6db5fb47f2c658fcfa4148b8f49e0775062cfb53
yibing <472621811@qq.com>
2015-10-29 16:34:36 +0800

    o Signed-off-by:yibing <472621811@qq.com>

--- application/views/foo_message.php
+++ application/views/foo_message.php
@@ -33,7 +33,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <body>
 
         <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="width:100%">
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
